a044d62123c36c3e1ccf6fd3fe5b1aab43b9cb8d
Frem <kor7781@gmail.com>
2023-06-27 20:53:14 +0300

    fix attack event

--- class_game/GameEvents.h
+++ class_game/GameEvents.h
@@ -1,11 +1,24 @@
 #pragma once
 #include "Actor.h"
+#include <vector>
+#include <iostream>
 namespace GameEvents {
-	void attack(Frem::Actor attacker, Frem::Actor attaked)
-	{
-		double damage = attacker.get_damage();
-		double health = attaked.get_health();
-		double res = health - damage;
-		attaked.set_health(res);
-	}
+
+    struct AttackData {
+        Frem::Actor* attacker;
+        Frem::Actor* attacked;
+        double damage;
+    };
+
+    std::vector<AttackData> attack(Frem::Actor* attacker, Frem::Actor* attacked)
+    {
+        double damage = attacker->get_damage();
+        double health = attacked->get_health();
+        double res = health - damage;
+        AttackData data = { attacker, attacked, res };
+        attacked->set_health(res);
+        std::cout << attacker->get_name() << " hit " << attacked->get_name() << " by " << damage << std::endl;
+        return { data };
+    }
+
 }
--- class_game/class_game.cpp
+++ class_game/class_game.cpp
@@ -2,13 +2,16 @@
 #include "Actor.h"
 #include "PLayer.h"
 #include "GameEvents.h"
+
 int main()
 {
-	Frem::Actor a("Player 1", true, 100.1, 5.1);
-	Frem::Actor b("Player 2", false, 15.1, 12.1);
+	Frem::Actor Player_1("Player 1", true, 100.1, 5.1);
+	Frem::Actor Player_2("Player 2", false, 15.1, 12.1);
 
+	std::vector<GameEvents::AttackData> attack_data = GameEvents::attack(&Player_1, &Player_2); // вызываем функцию attack и сохраняем результат
 	
-	GameEvents::attack(b, a);
-	a.get_info();
-	b.get_info();
+	Player_1.get_info();
+	Player_2.get_info();
+
+	return 0;
 }
